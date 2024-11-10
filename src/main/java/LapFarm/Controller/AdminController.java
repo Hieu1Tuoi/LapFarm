d634b47753b3e208294eed5b6b6247ab5a4e5840
NgocnhatBruh <ngocnhatbruh@gmail.com>
2024-11-10 16:14:00 +0700

    vailon

--- src/main/java/LapFarm/Controller/AdminController.java
+++ src/main/java/LapFarm/Controller/AdminController.java
@@ -5,10 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-	@RequestMapping(value = { "/admin", "/admin/*" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "" }, method = RequestMethod.GET)
 	public String admin() {
 
-		return "admin";
+		return "admin/index";
+	}
+	
+	@RequestMapping(value = { "/menu" }, method = RequestMethod.GET)
+	public String menu() {
+
+		return "admin/menu";
 	}
 }
